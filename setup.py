edcac1ffc92caa2182aa0c82ad4e15eb8f4f8911
Takao KOBAYASHI <tkoba1974@gmail.com>
2021-03-20 17:17:38 +0900

    ./setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from __future__ import absolute_import, division, print_function
 
 ########################################################################################################################
 ### Do not forget to adjust the following variables to your own plugin.
